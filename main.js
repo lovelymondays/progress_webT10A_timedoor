dff56b3fbbc9b1ea61842db4c8885626e6004092
lovelymondays <arjism23@gmail.com>
2024-01-26 09:18:26 +0700

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+function getInTouch() {
+  const name = "Arji Maulana";
+  const title = "Web Developer";
+  const phone = "+62 882 9551 3038";
+  const email = "arjism23@gmail.com";
+  const url = "https://arjism.com";
+}
+
+getInTouch();
